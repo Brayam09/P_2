29acbe23e0b0c65050455afda6c51b1fa426d856
Brayam09 <u20211198882@usco.edu.co>
2023-08-08 18:40:11 -0500

    Cambio

---
+++ Prueba.py
@@ -0,0 +1,23 @@
+a = 40
+b = 12
+
+print("El primer número es: ", a)
+print("El segundo número es: ", b)
+
+suma = a + b
+print ("El resultado de la suma es:", suma)
+
+resta = a - b 
+
+print ("El resultado de la resta es:", resta)
+
+división = a/b
+
+print ( "El resultado de la división es: ", división)
+
+multiplicación = a * b
+
+print ( "El resultado de la multiplicación es: ", multiplicación)
+
+
+

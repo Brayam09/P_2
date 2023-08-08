cd2e8ab7d8a7be54af50a992924ce19526e29d7e
Brayam09 <u20211198882@usco.edu.co>
2023-08-08 18:57:20 -0500

    Ejercicio clase 08/08/2023

--- Prueba.py
+++ Prueba.py
@@ -19,5 +19,34 @@ multiplicación = a * b
 
 print ( "El resultado de la multiplicación es: ", multiplicación)
 
+# ----------------------------------------------------------------------- 
+
+
+a = int(input("Ingrese un primer numero:"))
+b = int(input("Ingrese un segundo numero:"))
+
+print("El primer número es: ", a)
+print("El segundo número es: ", b)
+
+suma = a + b
+print ("El resultado de la suma es:", suma)
+
+resta = a - b 
+
+print ("El resultado de la resta es:", resta)
+
+división = a/b
+
+print ( "El resultado de la división es: ", división)
+
+multiplicación = a * b
+
+print ( "El resultado de la multiplicación es: ", multiplicación)
+
+modulo = a % b 
+
+
+print ( "El modulo de la divison es: ", modulo)
+
 
 

2db7516084aa3610582762a4a4d0fcc32e385c76
Brayam09 <u20211198882@usco.edu.co>
2023-08-09 17:30:22 -0500

    Cambio bliblio

--- Prueba.py
+++ Prueba.py
@@ -1,24 +1,23 @@
-a = 40
-b = 12
+# a = 40
+#b = 12
 
-print("El primer número es: ", a)
-print("El segundo número es: ", b)
+#print("El primer número es: ", a)
+#print("El segundo número es: ", b)
 
-suma = a + b
-print ("El resultado de la suma es:", suma)
+#suma = a + b
+#print ("El resultado de la suma es:", suma)
 
-resta = a - b 
+#resta = a - b 
 
-print ("El resultado de la resta es:", resta)
+#print ("El resultado de la resta es:", resta)
 
-división = a/b
+#división = a/b
 
-print ( "El resultado de la división es: ", división)
+#print ( "El resultado de la división es: ", división)
 
-multiplicación = a * b
+#multiplicación = a * b
 
-print ( "El resultado de la multiplicación es: ", multiplicación)
-
+#print ( "El resultado de la multiplicación es: ", multiplicación)
 # ----------------------------------------------------------------------- 
 
 
@@ -35,7 +34,7 @@ resta = a - b
 
 print ("El resultado de la resta es:", resta)
 
-división = a/b
+división = int(a/b)
 
 print ( "El resultado de la división es: ", división)
 

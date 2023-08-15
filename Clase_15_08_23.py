32018f5a9a5919666bc06a2ad3d712be061accf7
Brayam09 <u20211198882@usco.edu.co>
2023-08-15 18:36:47 -0500

    Ciclo For

--- Clase_15_08_23.py
+++ Clase_15_08_23.py
@@ -13,3 +13,18 @@ b = 0
 while b < len(listado):
     print(listado[b])
     b +=1
+
+
+### For
+
+
+frutas= ["limon","fresa", "pera", "manzana"]
+
+for i in frutas:
+    print(i)
+
+for a in range(10):
+    print(a)
+    
+for a in range (2, 20, 2):
+    print(a)

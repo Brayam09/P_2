6ec6d0eccf963a3b954ae3687d4c6e61fd3bdbe0
Brayam09 <u20211198882@usco.edu.co>
2023-08-15 18:14:22 -0500

    Ciclo While

---
+++ Clase_15_08_23.py
@@ -0,0 +1,15 @@
+#while
+
+varcontrol = 1
+
+while varcontrol <= 10 :
+    print(varcontrol)
+    varcontrol +=1
+
+listado = [ 'juan', 'pedro' , 'maria', 'sofia' ]
+
+b = 0
+
+while b < len(listado):
+    print(listado[b])
+    b +=1

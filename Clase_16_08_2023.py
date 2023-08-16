1198a444c0ad171bddf227347bb3b192ff130dfc
Brayam09 <u20211198882@usco.edu.co>
2023-08-16 18:41:08 -0500

    Eliminar y añadir

--- Clase_16_08_2023.py
+++ Clase_16_08_2023.py
@@ -20,11 +20,66 @@ q = 0
 while q < w :
     print(varlista[q])
     q +=1
-"""
+
 q = 0
 
 while q < len(varlista):
     print(varlista[q])
     q +=1
+
+"""
+"""
+#Eliminar elementos
+
+del varlista[0]
+
+q = 0
+
+while q < len(varlista):
+    print(varlista[q])
+    q +=1
+"""
+"""
+eliminar = input("Ingrese elemento a elminar: ")
+contador = 0
+
+for z in varlista:
+    if eliminar == z :
+        del varlista[contador]
+
+    else :
+        contador +=1
+    
+print(varlista)
+
+
+#Agregar elementos
+
+print()
+
+varlista.insert(2,"Clase")
+
+
+for z in varlista:
+    print(z)
+
+print()
+
+varlista.append("6")
+
+
+for z in varlista:
+    print(z)
+
+
+print()
+
+varlista.append(input("Ingrese un nombre: "))
+
+
+for z in varlista:
+    print(z)
+"""
+
 
 

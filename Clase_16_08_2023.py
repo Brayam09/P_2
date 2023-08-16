c8852cae076cebd5c81dd9cc175eb8f116f7d7a9
Brayam09 <u20211198882@usco.edu.co>
2023-08-16 18:08:13 -0500

    Listas

---
+++ Clase_16_08_2023.py
@@ -0,0 +1,30 @@
+#LISTAS
+
+
+varlista = ['Hector',True,5.5,9]
+"""
+print(varlista[0])
+print(varlista[1])
+print(varlista[2])
+print(varlista[3])
+
+for z in varlista:
+    print(z)
+
+w = len(varlista)
+
+print(w)
+
+q = 0
+
+while q < w :
+    print(varlista[q])
+    q +=1
+"""
+q = 0
+
+while q < len(varlista):
+    print(varlista[q])
+    q +=1
+
+
